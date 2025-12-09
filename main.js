07228a22d4b68a0382339605aac51e6b79f223ec
AeliusDesigns <aelius.gaming413@gmail.com>
2025-12-09 15:40:06 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
+import * as THREE from "https://unpkg.com/three@0.161.0/build/three.module.js";
+import { OrbitControls } from "https://unpkg.com/three@0.161.0/examples/jsm/controls/OrbitControls.js";
+
 <script type="module" src="./main.js"></script>
